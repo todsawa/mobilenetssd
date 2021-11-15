41ac7e117543335b92eeae1bdff45718cd396547
todsawa <89174643+todsawa@users.noreply.github.com>
2021-11-15 14:53:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -149,7 +149,14 @@ def event_handle(event):
 
     if msgType == "text":
         msg = str(event["message"]["text"])
-        replyObj = TextSendMessage(text=msg)
+        if msg == "สวัสดี" :
+            replyObj = TextSendMessage(text="สวัสดีจ้า")
+        elif msg == "กินข้าวรึยัง" : 
+            replyObj = TextSendMessage(text="ยังไม่ได้กิน")
+        elif msg == "ง่วงนอนไหม" :
+            replyObj = TextSendMessage(text="ยังไม่ง่วง")
+        else :
+            replyObj = TextSendMessage(text=msg)
         line_bot_api.reply_message(rtoken, replyObj)
     elif msgType == "image":
         try:
